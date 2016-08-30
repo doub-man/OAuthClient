b3f019e46b95e17c3f77395b874e6467e648f536
ZengWeimao <so.zengtao@gmail.com>
2016-08-30 22:42:27 +0800

    init oauth2 client

---
+++ lib/omniauth/strategies/todo.rb
@@ -0,0 +1,27 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Todo < OmniAuth::Strategies::OAuth2
+      option :name, :todo
+
+      option :client_options, {
+        site: 'http://localhost:3000',
+        authorize_path: "/oauth/authorize"
+      }
+
+      uid do
+        raw_info["id"]
+      end
+
+      info do
+        { email: raw_info["email"] }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get("/api/me").parsed
+      end
+
+    end
+  end
+end
